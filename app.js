9cb1810d46bbfc1cc21576ad3015048dc65f4645
Dasol Park <dasol.park615@gmail.com>
2019-09-26 20:01:07 +0900

    2-2. Recap!

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -22,17 +25,13 @@ function onMouseMove(event) {
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
-    ctx.strokeStyle();
+    ctx.stroke();
   }
 }
 
-function onMouseDown(event) {
-  painting = true;
-}
-
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
 }
